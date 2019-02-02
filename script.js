a162ef9402496c8be755dbccb7dbfe74d697296d
Kevin Krato <kevinkrato@Kevins-MacBook-Pro.local>
2019-02-01 23:21:40 -0700

    Further implement more functionality and condition testing to buttons

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 /* ==== QUERIES ==== */
 
+
+// All these querySelectors return a static (not live) Array-like object.
 /* =Range Queries= */
 let minRange = document.querySelector('.min-range');
 let maxRange = document.querySelector('.max-range');
@@ -30,6 +32,44 @@ let btnReset = document.querySelector('.reset-btn');
 let btnClear = document.querySelector('.clear-btn');
 
 
+
+btnClear.addEventListener('click', function() {
+	function clearTheInputs() {
+		function checkInputs() {
+			if (minRange.value.length > 0 || maxRange.value.length > 0 || ch1NameInput.value.length > 0 || ch2NameInput.value.length > 0 || 
+				ch1Guess.value.length > 0 || ch2Guess.value.length > 0) {
+				emptyTheInput(allInputs);
+				console.log(trueRandomNumber);
+			} else {
+				console.log('CLEAR INVALID');
+			}
+		}
+		checkInputs();
+	}
+	clearTheInputs();
+	revertAllOutputs();
+})
+
+//keeping it in outer scope in the event future / other implementations need access to functionality.
+
+btnReset.addEventListener('click', function() {
+	emptyTheInput(allInputs);
+	resetRandomNumber();
+	revertAllOutputs();
+	//resetRandomNumber needs to return a new random Number;
+	function resetRandomNumber() {
+		return trueRandomNumber = generateRandomNum(minRange, maxRange)
+		console.log(trueRandomNumber);
+	}
+})
+
+
+
+
+
+
+
+
 btnUpdate.addEventListener('click', function() {
 	checkForNumbers();
 
@@ -37,7 +77,7 @@ btnUpdate.addEventListener('click', function() {
 		if ( (minRange.value.length > 0 && maxRange.value.length > 0) && ( parseInt(minRange.value) < parseInt(maxRange.value) ) ) {
 			rangeLow.innerText = minRange.value;
 			rangeHigh.innerText = maxRange.value;
-			return generateRandomNum(minRange.value, maxRange.value);
+			trueRandomNumber = generateRandomNum(minRange.value, maxRange.value);
 		} else {
 			console.log('false');
 			console.log(minRange.value)
@@ -52,10 +92,13 @@ btnSubmit.addEventListener('click', function() {
 
 	function checkForNumbers() {
 		if ( ch1Guess.value.length > 0 && ch2Guess.value.length > 0 ) {
+			// console.log("1output is: " + ch1Guess[0] );
+			// console.log("2output type is: " + typeof(ch1Guess));
+			// // console.log("3output array is: " + Array.isArray(ch1Guess));
 			ch1CurGuess.innerText = ch1Guess.value;
 			ch2CurGuess.innerText = ch2Guess.value;
 		} else {
-			console.log('false');
+			alert("Please enter valid names and inputs in all the fields.")
 		}
 	}
 
@@ -64,6 +107,7 @@ btnSubmit.addEventListener('click', function() {
 		if ((invalidName.test(ch1NameInput.value) || (ch1NameInput.value.length < 1) 
 			|| (invalidName.test(ch2NameInput.value) || (ch2NameInput.value.length < 1)))) {
 			console.log('name is invalid');
+			alert("Please enter alpha-numeric names only.")
 		} else {
 			updateAll(ch1NameInput, ch1NameOutput);
 			updateAll(ch2NameInput, ch2NameOutput);
@@ -73,27 +117,76 @@ btnSubmit.addEventListener('click', function() {
 });
 
 
+
+/*=== TEMPORARILY GLOBAL FUNCTIONS && VARIABLES ====*/
+
+let trueRandomNumber;
+let allInputs = Array.from(document.getElementsByTagName('input'));
+
 function generateRandomNum(minRange, maxRange) {
+	minRange = rangeLow.innerText;
+	maxRange = rangeHigh.innerText;
 	let randomNum = Math.floor((Math.random() * maxRange) + minRange) +1;
 	console.log(randomNum)
+	return randomNum;
 }
 
+
+// function emptyTheInput(element) {
+// 	element.value = '';
+// }
+
+
+function emptyTheInput(array) {
+	for (let i = 0; i < array.length; i++) {
+		array[i].value = '';
+	}
+}
+
+function revertAllOutputs() {
+	updateAll(null, ch1NameOutput, 'Challenger 1 Name');
+	updateAll(null, ch2NameOutput, 'Challenger 2 Name');
+	ch1CurGuess.innerText = 0;
+	ch2CurGuess.innerText = 0;
+}
+
+//target specific DOM outputs with specific DOM inputs
+function updateAll(query, output, string) {
+ 	if (query === null) {
+ 		return changeContent(null, output, string);
+ 	} else {
+ 		let qValue = query.value;
+ 		return changeContent(qValue, output, string);
+ 	}
+}
+function changeContent(input, output, string) {
+	// console.log("input is: " + input );
+	// console.log("string argument is : " + string);
+	let changeHtml;
+	if (input === null) {
+		changeHTML = element => element.innerHTML = string;
+	} else {
+		changeHTML = element => element.innerHTML = input;
+	}
+	output.forEach(changeHTML);
+}
+
+
+
+
+
+// function updateAll(query, output, string) {
+//  	let qValue = query.value;
+// 	return changeContent(qValue, output, string);
+// }
+// function changeContent(input, output, string) {
+// 	console.log("input is: " input );
+// 	let changeHTML = element => element.innerHTML = input;
+// 	output.forEach(changeHTML);
+// }
 // updateColorRed(errorInputName, errorInputNumb, ch1NameOutput)
 // updateVisibility(errorInputName);
 
-
-
-// ==== FUNCTIONALITY ==== //
-
-//target specific outputs with specific inputs
-function updateAll(query, output) {
- 	let qValue = query.value;
-	return changeContent(qValue, output);
-}
-function changeContent(input, output) {
-	let changeHTML = element => element.innerHTML = input;
-	output.forEach(changeHTML);
-}
 
 
 
